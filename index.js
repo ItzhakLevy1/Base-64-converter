5c0a840aecb1ab87f6c7aa3d2c971c92180fcce2
ItzhakLevy1 <91373674+ItzhakLevy1@users.noreply.github.com>
2022-02-26 23:39:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
   function onload() { 
-      input = document.getElementById("input"); 
-      showRevealedText = document.getElementById("showRevealedText");
+    textToDecode = document.getElementById("textToDecode"); 
+    showDecodedText = document.getElementById("showDecodedText");
+    
+    textToEncode = document.getElementById("textToEncode"); 
+    showEncodedText = document.getElementById("showEncodedText");
   }
 
-  function convertBase64(){
-      let incodedStr = input.value;
-      let decodedStr = atob(incodedStr);
-      showRevealedText.innerText = decodedStr;  // Displaying the input value .
+  function encodingBase64() {
+    let toEncodeStr = textToEncode.value;
+    let encodedStr = btoa(toEncodeStr);
+    showEncodedText.innerText = encodedStr;
   }
-
-
+  
+  function decodeingBase64() {
+    let encodedStr64 = textToDecode.value;
+    let decodedStr = atob(encodedStr64);
+    showDecodedText.innerText = decodedStr;  // Displaying the input value .
+}
